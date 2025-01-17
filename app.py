858a3e78d31710f144728d69cce4bb8d1786769e
RuanFelixx <ruan.felix2024@gmail.com>
2025-01-16 22:46:08 -0300

    fazendo telas de login e cadastro

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 from gestao_pedidos import app
 from gestao_pedidos.controllers import UserController
+
+#TESTES PARA EDITAR O HTML E CSS
+#obs: caso queira colocar em outro lugar, pode colocar 
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
