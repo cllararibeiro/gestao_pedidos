08f4b65d0e7869ea71de68b806839616e1befd28
Davignbrt <161861913+Davignbrt@users.noreply.github.com>
2025-01-23 11:31:22 +0000

    mudanças feitas no app e relatorios.html

--- app.py
+++ app.py
@@ -57,18 +57,49 @@ def relatorios():
         intervalo = request.form['intervalo']
         
         if nome and start_data and final_data:
-            pass
+            #total de pedidos em reais por cliente em um intervalo de datas 
+            cursor = mysql.connection.cursor()
+            query = "SELECT ped_total FROM tb_pedidos JOIN tb_clients ON cli_id = ped_cli_id WHERE cli_nome = %s AND ped_data BETWEEN %s AND %s"
+            cursor.execute(query, (nome, start_data, final_data))
+            resultado = cursor.fetchall()
+            cursor.close()
 
-        if valor_pedido and start_data and final_data:
-            pass
+        elif valor_pedido and start_data and final_data:
+            #clientes com pedidos acima de determinado valor ex: 500 R$
+            cursor = mysql.connection.cursor()
+            query = "SELECT ped_total FROM tb_pedidos JOIN tb_clients ON cli_id = ped_cli_id WHERE ped_total > %s AND ped_data BETWEEN %s AND %s"
+            cursor.execute(query, (valor_pedido, start_data, final_data))
+            resultado = cursor.fetchall()
+            cursor.close()
 
-        if tops and intervalo:
-            pass
-            
-        if intervalo:
-            pass
+        elif tops and intervalo:
+            # top produtos nos intervalos de tempo de 7, 30, 60 e 90 dias 
+            cursor = mysql.connection.cursor()
+            query = """
+                SELECT proPed_qdproduto, pro_nome FROM tb_proped JOIN tb_produtos ON proPed_pro_id = pro_id 
+                WHERE proPed_data BETWEEN DATE_SUB(NOW(), INTERVAL %s DAY) AND NOW()
+                ORDER BY proPed_qdproduto DESC
+                LIMIT %s
+            """
+            cursor.execute(query, (nome, start_data, final_data))
+            resultado = cursor.fetchall()
+            cursor.close()
 
-    return "essa rota esta em manutenção"
+        elif intervalo:
+            #produtos não pedidos em intervalos de tempo de 7, 30, 60 e 90 dias 
+            cursor = mysql.connection.cursor()
+            query = """
+                SELECT pro_nome FROM tb_produtos WHERE pro_id NOT IN (
+                    SELECT proPed_pro_id 
+                    FROM tb_proped 
+                    WHERE proPed_data BETWEEN DATE_SUB(NOW(), INTERVAL %s DAY) AND NOW()
+                )
+            """
+            cursor.execute(query, (nome, start_data, final_data))
+            resultado = cursor.fetchall()
+            cursor.close()
+
+    return render_template('relatorios.html', resultado = resultado)
 
 @app.route('/cadastrar_produto', methods=['GET', 'POST'])
 def cadastrar_produto():
