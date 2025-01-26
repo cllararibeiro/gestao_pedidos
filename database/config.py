28e9dd653686a6ef05270d2a657d03371b8c56f7
Davignbrt <daviguedesnobertoguedes@gmail.com>
2025-01-26 19:34:27 -0300

    atualizei

--- database/config.py
+++ database/config.py
@@ -9,7 +9,7 @@ app.secret_key = str(os.getenv("SECRET_KEY"))
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 app.config['MYSQL_CURSORCLASS'] = os.getenv("MYSQL_CURSORCLASS")
 
